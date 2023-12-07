399adee2e1f4cdd45f896f95b278824b569c6faa
Ajay Chauhan <92268333+black-devil-369@users.noreply.github.com>
2023-12-07 14:41:39 +0000

    app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+// import Header from './component/header/Header'
+import Allpages from './component/Allpages'
+import './App.css'
+function App() {
+  return (
+    <>
+    <Allpages/>
+    </>
+  )
+}
+
+export default App
